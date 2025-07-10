6c4add0e9028751e92a1fba6c0e7f44f84f61bb6
Minahil <169134882+minahil2674@users.noreply.github.com>
2025-07-10 15:42:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const btn =document.querySelector('#btn');
+
+
+const getColor = () =>{
+    const randomNumber = Math.floor(Math.random() * 16777215)
+    const randomCode = "#" + randomNumber.toString(16)
+    console.log(randomNumber, randomCode);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById('colorCode').innerText = randomCode;
+
+    navigator.clipboard.writeText(randomCode)
+}
+
+
+btn.addEventListener('click', ()=>{
+        getColor();
+})
+
+getColor();
